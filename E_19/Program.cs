039f2bb5e7027c6a0fdbe024dbb7dab91c32e013
Violetta <viopria@gmail.com>
2022-09-09 21:17:34 +0300

    E_19

---
+++ E_19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine( "число палиндром" );
+  }
+  else Console.WriteLine( "число не палиндром" );
+}
+ if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine("не пятизначное число");
+
+Console.ReadKey();
+
