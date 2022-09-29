5260b1f2e38cad352fffe2b681043462bcde9431
Violetta <viopria@gmail.com>
2022-09-29 23:49:46 +0300

    E_68

---
+++ E_68/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static int Depth = 0;
+        public static UInt64 AckermannFunc(UInt64 m, UInt64 n) {
+            Depth++;
+            if (m == 0)
+                return n + 1;
+            if (n == 0)
+                return AckermannFunc(m - 1, 1);
+            return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+        }
+ 
+        static void Main(string[] args) {
+            Console.WriteLine(AckermannFunc(3, 5));
+            Console.WriteLine(Depth);
+        }
+    }
+}
