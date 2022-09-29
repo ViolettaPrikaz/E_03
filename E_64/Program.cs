bd022e29f149fc2275d8429d9a4c362c8a07650e
Violetta <viopria@gmail.com>
2022-09-29 23:50:05 +0300

    E_64

---
+++ E_64/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApplication17
+{
+    class Program
+    {
+        static string KO(int n)
+        {
+            if (n == 1)
+                return "1";
+            else
+                return KO(n - 1) + n;
+        }
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(KO(3));
+ 
+        }
+    }
+}
